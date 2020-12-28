bcfe3bf9d8995d1a7423b1d8711c0ef91a1b3d59
Neprav <57597189+Neprav@users.noreply.github.com>
2020-12-28 19:19:16 +0400

    Update backuper.py

--- backuper.py
+++ backuper.py
@@ -1,7 +1,6 @@
 from pathlib import *
 import shutil
 
-
 dirs = [{
 'main_dir': Path('I:\\САЙТЫ ДЛЯ ФОТОГРАФОВ'),
 'backup_dir': Path('H:\\Б - САЙТЫ ДЛЯ ФОТОГРАФОВ')},
@@ -10,36 +9,37 @@ dirs = [{
 'backup_dir': Path('H:\\Б - Python')},
 ]
 
-count_f, count_d = 0, 0
+count_files, count_dirs = 0, 0
 
-def path_assemble(pth):
-	path = backup_dir
-	parts = list(pth.parts[len_main_dir:])
-	for part in parts: 
-		path = path / part
-	return path
+# Собираем путь 
+def path_assemble(path):
+	new_path = backup_dir
+	parts = list(path.parts[len_main_dir:])
+	for part in parts:
+		new_path = new_path / part
+	return new_path
 
-def dipping (path):
-	global count_f
-	global count_d
-	for pth in path.iterdir():		
-		if pth.is_dir():
-			copy_dir = path_assemble(pth)
+# Рекурсивно погружаемся в каталоги
+def dipping(path):
+	global count_files
+	global count_dirs
+	for path in path.iterdir():		
+		if path.is_dir():
+			copy_dir = path_assemble(path)
 			if not copy_dir.exists():
 				copy_dir.mkdir()				
-				print(f'Создана папка "{pth.name}"')
-				count_d += 1	
-			dipping(pth)
+				print(f'Создана папка "{path}"')
+				count_dirs += 1	
+			dipping(path)
 		else:
-			copy_file = path_assemble(pth)
-			if not copy_file.exists() or pth.stat().st_mtime > copy_file.stat().st_mtime:
+			copy_file = path_assemble(path)
+			if not copy_file.exists() or path.stat().st_mtime > copy_file.stat().st_mtime:
 				try:
-					shutil.copy(pth, copy_file)
+					shutil.copy(path, copy_file)
 				except PermissionError:
-					print(f'Ошибка доступа! "{pth.name}"')
-				print(f'Скопирован файл "{pth.name}"')
-				count_f += 1
-
+					print(f'Ошибка доступа! "{path}"')
+				print(f'Скопирован файл "{path}"')
+				count_files += 1
 
 for item in dirs:
 	main_dir = item['main_dir']
@@ -47,16 +47,5 @@ for item in dirs:
 	len_main_dir = len(main_dir.parts)
 	dipping(main_dir)
 
-
-print(f'Создано {count_d} папок, скопировано {count_f} файлов')
-
-
-
-
-
-
-
-
-
-
-
+print('====================================')
+print(f'Создано {count_dirs} папок, скопировано {count_files} файлов')
