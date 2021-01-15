4eeeef55368610bbb0fc8984f3c92b2ceaf49e78
Neprav <57597189+Neprav@users.noreply.github.com>
2021-01-15 18:44:57 +0400

    Create rename-date.py

---
+++ rename-date.py
@@ -0,0 +1,26 @@
+from pathlib import *
+import re
+
+path = Path(r'Путь')
+date_mask = r'\d{2}.\d{2}.\d{4}'
+
+for folder in path.iterdir():
+	if folder.is_dir():
+		path_parent = folder.parent
+		name = folder.name
+		print('Было:  ', folder)
+		date = re.search(date_mask, name)
+		if date:
+			date = date.group(0)
+			name = name.replace(date, '')
+			name = name.replace('—', '')			
+			date_split = date.split('.')
+			date_inverse = date_split[::-1]
+			date = '.'.join(date_inverse)			
+			name = date + ' — ' + name
+			name = re.sub(' +', ' ', name)
+			new_path = folder.parent / name
+			print('Стало: ', new_path)
+			folder.rename(new_path)
+		else:
+			print('Стало: ', folder)
